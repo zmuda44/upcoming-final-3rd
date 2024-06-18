b9867eeb00908599955ab13a5ae332910daacc57
Greg <gregbailey617@gmal.com>
2024-06-18 13:22:18 -0400

    replaced messed up one

--- work files module 17 cs work files/04-Stu_Functional-Execution-Context/Unsolved/index.js
+++ work files module 17 cs work files/04-Stu_Functional-Execution-Context/Unsolved/index.js
@@ -1,22 +1,13 @@
 // returns the average value of an array of numbers
-
-
 function avg(array) {
-  
   function sum() {
     let total = 0;
     for(let i = 0; i < array.length; i++) {
       total += array[i];
     }
-    return total
   }
   
-  const totalSum = sum(array)
-  
-  let average = totalSum/array.length;
-  console.log(average)
+  return total/array.length;
 }
-
-avg([10, 20, 30, 40, 50, 30])
 
 module.exports = avg;
